11a1b2f8e50327b422b92285a77800eb9c69c9d1
Aaron Wang <aaron_wang@apple.com>
2014-05-31 22:46:57 -0700

    Added matches/detail

---
+++ app/controllers/matches_controller.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'net/http'
+require 'uri'
+
+class MatchesController < ApplicationController
+  def detail
+  	if params[:match_id]
+		match_id = params[:match_id]
+	else
+		match_id = "27110133"
+	end
+    url = Dota2Stats::Application::STEAM_MATCH_DETAIL + '&match_id=' + match_id
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    @players = JSON.parse(response.body)['result']['players']
+  end
+end
