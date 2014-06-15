aaf5b8d8c64aacfa905c3194c354e0a17053875c
Jeph Do <jdo@solarcity.com>
2014-06-15 15:54:51 -0700

    add home page to search for playerid

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -3,10 +3,18 @@ require 'net/http'
 require 'uri'
 
 class PlayerController < ApplicationController
+  def index
+  end
+
   def recent
     @player_id = params[:player_id]
+    puts @player_id
+    puts params
     uri = Dota2Stats::Application::STEAM_MATCH_URL #+ '&account_id=' + player_id
+    uri = uri + '&account_id=' + @player_id
     uri = URI.parse(uri)
+
+    puts uri
 
     response = Net::HTTP.get_response(uri)
     matches = JSON.parse(response.body)['result']['matches']
@@ -32,5 +40,14 @@ class PlayerController < ApplicationController
 
         @matches.push(m)
     end
+  end
+
+  def overview
+    @player_id = params[:player_id]
+    uri = Dota2Stats::Application::STEAM_MATCH_URL #+ '&account_id=' + player_id
+    uri = URI.parse(uri)
+
+    response = Net::HTTP.get_response(uri)
+    matches = JSON.parse(response.body)['result']['matches']
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root :to => 'player#index'
+  
   get 'player/:player_id' => 'player#recent'
 
   get 'matches/overview'
